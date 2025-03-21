5c8053f02fb03b8444964ecc0ebd9849fb554279
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2025-03-21 19:11:14 -0300

    ♻️ Refactor PicSelector logic into RandomPicSelector
    Remove the abstract PicSelector class and move its logic directly into
    RandomPicSelector. Simplify directory traversal and image filtering
    by introducing helper functions (`getAllImages` and `isImage`). This
    reduces complexity and improves maintainability.

--- src/main/kotlin/br/com/colman/pikframe/picselector/RandomPicSelector.kt
+++ src/main/kotlin/br/com/colman/pikframe/picselector/RandomPicSelector.kt
@@ -6,15 +6,25 @@ import kotlinx.coroutines.flow.flow
 import java.io.File
 import kotlin.random.Random
 
-class RandomPicSelector(directory: File) : PicSelector(directory) {
-  private var values = children.toMutableList()
-  
-  
-  override val pictures = flow { 
+class RandomPicSelector(private val directory: File) {
+  private var children = directory.getAllImages().toMutableList()
+
+  init {
+    if (!directory.isDirectory) throw IllegalArgumentException("The provided path must be a directory")
+  }
+
+  val pictures = flow {
     while (true) {
       delay(PikFrameConfig.picSelector.emitPictureDelay.toMillis())
-      emit(values.removeAt(Random.nextInt(values.size)))
-      if(values.isEmpty()) values = children.toMutableList()
+      emit(children.removeAt(Random.nextInt(children.size)))
+      if (children.isEmpty()) children = directory.getAllImages().toMutableList()
     }
   }
 }
+
+private fun File.getAllImages(): List<File> {
+  if (this.isDirectory) return listFiles().flatMap { it.getAllImages() }.distinct()
+  return listOf(this).filter(File::isImage)
+}
+
+private fun File.isImage() = extension.lowercase() in listOf("jpg", "jpeg", "png", "bmp", "webp")
